4f82bf31b3f3b867296f9fdbc53fe5c9c7b1b36e
David <grayd500@gmail.com>
2023-09-13 12:49:54 -0500

    update with brewery search

--- script.js
+++ script.js
@@ -1,23 +1,100 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let gameState = "start";
-    document.getElementById("start-button").addEventListener("click", gameLoop);
+// These lines link HTML elements and stores them a variable.
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+// This function gets info from the URL.
+function getParams() {
+  var searchParamsArr = document.location.search.split('&');
+
+  if (searchParamsArr[0] && searchParamsArr[1]) {
+    var query = searchParamsArr[0].split('=').pop();
+    var format = searchParamsArr[1].split('=').pop();
+    searchApi(query, format);
+  }
+}
+
+function printResults(brewery) {
+  // Make a new box to put the brewery info in
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  // Make another box inside the first one
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  // Show the brewery's name as the title
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = brewery.name;
+  resultBody.append(titleEl);
+
+  // Show the brewery's type
+  var typeEl = document.createElement('p');
+  typeEl.textContent = 'Type: ' + brewery.brewery_type;
+  resultBody.append(typeEl);
+
+  // Add the whole box to the results area on the webpage
+  resultContentEl.append(resultCard);
+}
+
+
+async function searchApi(postalCode, type) {
+  // Log the parameters for debug purposes
+  console.log("Search Params:", { postalCode, type });
+
+  var apiUrl = 'https://api.openbrewerydb.org/breweries';
+  apiUrl += '?by_postal=' + postalCode + '&by_type=' + type;
   
-    function gameLoop() {
-      if (gameState === "start") {
-        document.getElementById("game-text").innerText = "You find yourself in a mysterious brewery. Do you go to the 'Taproom' or 'Brewery Floor'?";
-        gameState = "brewery";
-      } else if (gameState === "brewery") {
-        const choice = prompt("Do you go to the 'Taproom' or 'Brewery Floor'?");
-        if (choice.toLowerCase() === "taproom") {
-          document.getElementById("game-text").innerText = "Cheers! You find yourself among jovial beer enthusiasts. You've been handed a free pint of their special IPA! You win!";
-          gameState = "end";
-        } else if (choice.toLowerCase() === "brewery floor") {
-          document.getElementById("game-text").innerText = "Oops! You accidentally fall into a vat of unfinished stout. You've become part of the next brew! You lose!";
-          gameState = "end";
-        } else {
-          document.getElementById("game-text").innerText = "Invalid option. No beer for you until you decide!";
-        }
+  console.log(apiUrl);
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
+
+    const breweryList = await response.json();
+    // Check if the search returned any results
+    if (breweryList.length === 0) {
+      // Option 1: Show an alert
+      alert("No breweries of that type found in that ZIP. Please try a different postal code or type.");
+
+    } else {
+      resultContentEl.innerHTML = '';
+      for (var i = 0; i < breweryList.length; i++) {
+        printResults(breweryList[i]);
       }
     }
-  });
-  
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
+
+// This function is called when someone presses the "Search" button.
+function handleSearchFormSubmit(event) {
+  // It makes sure the page doesn't refresh.
+  event.preventDefault();
+
+  // It gets the search word and format someone typed into the boxes.
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  // If there's no search word, it shows an error.
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  // It does the search using the search word and format.
+  searchApi(searchInputVal, formatInputVal);
+}
+
+// When someone presses "Search," it calls the function to handle it.
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+// This starts the first function to get info from the web address.
+getParams();
